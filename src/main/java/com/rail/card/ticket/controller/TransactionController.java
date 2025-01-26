a5c328cadb56a902ba7479dacf6ad9f30143ff43
ida.sufiana <ida.sufiana@consultant.ioh.co.id>
2025-01-27 06:40:17 +0700

    add secure

--- src/main/java/com/rail/card/ticket/controller/TransactionController.java
+++ src/main/java/com/rail/card/ticket/controller/TransactionController.java
@@ -1,5 +1,7 @@
 package com.rail.card.ticket.controller;
 
+import com.rail.card.ticket.config.Secured;
+import com.rail.card.ticket.constant.ApplicationEnum;
 import com.rail.card.ticket.model.dto.HistoryDto;
 import com.rail.card.ticket.exception.TicketException;
 import com.rail.card.ticket.service.TransactionService;
@@ -17,21 +19,25 @@ public class TransactionController extends BaseController {
     @Autowired
     TransactionService transactionService;
 
+    @Secured({ApplicationEnum.Group.Admin})
     @PostMapping("/getbalance")
     public ResponseEntity<?> getbalance(@RequestHeader("Autorization") String authorization) throws TicketException {
         return success(transactionService.balance(getEmail(authorization)));
     }
 
+    @Secured({ApplicationEnum.Group.Admin})
     @PostMapping("/topUp")
     public ResponseEntity<?> topUp(@RequestHeader("Autorization") String authorization, @RequestBody Double amount) throws TicketException {
         return success(transactionService.topup(getEmail(authorization), amount));
     }
 
+    @Secured({ApplicationEnum.Group.Admin})
     @PostMapping("/transaction")
     public ResponseEntity<?> transaction(@RequestHeader("Autorization") String authorization, @RequestBody String serviceCode) throws TicketException {
         return success(transactionService.transaction(authorization, serviceCode));
     }
 
+    @Secured({ApplicationEnum.Group.Admin})
     @GetMapping("/history")
     public ResponseEntity<?> history(@RequestHeader("Autorization") String authorization, @RequestParam(required = false,defaultValue = "0") int page,
                                      @RequestParam(required = false,defaultValue = "100") int limit,
