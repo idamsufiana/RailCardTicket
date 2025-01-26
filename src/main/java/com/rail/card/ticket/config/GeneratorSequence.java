fb5e617f2c7afcb5c3cce73ccf47ec85c04bf721
ida.sufiana <ida.sufiana@consultant.ioh.co.id>
2025-01-26 12:13:54 +0700

    add sequence

---
+++ src/main/java/com/rail/card/ticket/config/GeneratorSequence.java
@@ -0,0 +1,7 @@
+package com.rail.card.ticket.config;
+
+public interface GeneratorSequence {
+    void create(String var1);
+
+    long get(String var1);
+}
---
+++ src/main/java/com/rail/card/ticket/config/GeneratorSequenceConfig.java
@@ -0,0 +1,20 @@
+package com.rail.card.ticket.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class GeneratorSequenceConfig {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+
+    @Bean
+    public GeneratorSequence generatorSequence() {
+        GeneratorSequence generatorSequence = new PsqlGeneratorSequence(jdbcTemplate);
+        generatorSequence.create("rail_seq");
+        return generatorSequence;
+    }
+}
